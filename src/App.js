6555d75506350d2db3de48c9d71c8ebc57be5c31
DmitrijKl <klyanchenkov.dmitrij@.mail.ru>
2024-04-06 17:31:14 +0400

    Added modal window

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import { useState } from "react";
 
 function App() {
+  const [cartIsVisible, setCartIsVisible] = useState(false);
   return (
     <>
+      <Cart />
       <Header></Header>
       <Meals></Meals>
     </>
---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./Cart.module.css";
+import Modal from "../UI/Modal";
+
+function Cart(props) {
+  const cartItems = (
+    <ul className={styles["cart-items"]}>
+      {[{ id: "m1", name: "Sushi", amount: 2, price: 10.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+  return (
+    <Modal>
+      {cartItems}
+      <div className={styles.total}>
+        <span>Итого</span>
+        <span>49.99</span>
+      </div>
+      <div className={styles.actions}>
+        <button className={styles["button--alt"]}>Закрыть</button>
+        <button className={styles.button}>Заказать</button>
+      </div>
+    </Modal>
+  );
+}
+
+export default Cart;
